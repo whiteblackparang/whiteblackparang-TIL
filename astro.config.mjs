c999a12178f898349588fa33ff7aba475d0861e7
Hun <hun5639@naver.com>
2026-07-08 14:40:47 +0900

    fix: base 경로 끝에 슬래시 추가

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://whiteblackparang.github.io',
-	base: '/whiteblack-TIL',
+	base: '/whiteblack-TIL/',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
